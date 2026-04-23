a179109249f1118734c54420b9182e51eb6d00a6
Yukile394 <muhamedmuhet@gmail.com>
2026-04-24 01:36:36 +0300

    Change FOV method to use float instead of double

--- src/main/java/exloran/silvera/mixin/GameRendererMixin.java
+++ src/main/java/exloran/silvera/mixin/GameRendererMixin.java
@@ -6,15 +6,11 @@ import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-/**
- * GameRendererMixin
- * - Zoom için FOV değiştirir
- */
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
 
     @ModifyReturnValue(method = "getFov", at = @At("RETURN"))
-    private double silvera_modifyFov(double original) {
-        return ZoomModule.getModifiedFov(original);
+    private float silvera_modifyFov(float original) {
+        return (float) ZoomModule.getModifiedFov(original);
     }
 }
