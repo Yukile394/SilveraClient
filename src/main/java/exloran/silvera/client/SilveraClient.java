f42ea52e2a833498a1681c2d33c94a5bd96843af
Yukile394 <muhamedmuhet@gmail.com>
2026-04-24 04:14:48 +0300

    Refactor package structure and update initialization log

--- src/main/java/exloran/silvera/client/SilveraClient.java
+++ src/main/java/exloran/silvera/client/SilveraClient.java
@@ -1,15 +1,16 @@
-package exloran.silvera.client;
+package com.silvera.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import exloran.silvera.hud.HudRenderer;
-import exloran.silvera.client.key.Keybinds;
+import com.silvera.client.hud.HudRenderer;
+import com.silvera.client.key.Keybinds;
 
 public class SilveraClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        HudRenderer.register();
         Keybinds.register();
-        System.out.println("Silvera Loaded.");
+        HudRenderer.register();
+
+        System.out.println("[Silvera] Client Loaded!");
     }
 }
