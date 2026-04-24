b94f5731d55b56850d6dcfa3afc76cd4789ce056
Yukile394 <muhamedmuhet@gmail.com>
2026-04-24 03:15:39 +0300

    Refactor HUD registration to use an interface

--- src/main/java/exloran/silvera/hud/HudRenderer.java
+++ src/main/java/exloran/silvera/hud/HudRenderer.java
@@ -1,4 +1,4 @@
-package com.silvera.client.hud;
+package exloran.silvera.hud;
 
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.gui.DrawContext;
@@ -7,9 +7,12 @@ public class HudRenderer {
 
     public static void register() {
 
-        HudRenderCallback.EVENT.register((DrawContext context, float tickDelta) -> {
-            ParticleHud.tick();
-            ParticleHud.render(context);
+        HudRenderCallback.EVENT.register(new HudRenderCallback() {
+            @Override
+            public void onHudRender(DrawContext context, float tickDelta) {
+                ParticleHud.tick();
+                ParticleHud.render(context);
+            }
         });
 
         System.out.println("HUD Registered");
