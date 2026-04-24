6ae6f5316721620b8a9028b8fef9643860ddd6ae
Yukile394 <muhamedmuhet@gmail.com>
2026-04-24 03:21:58 +0300

    Update package declaration in Keybinds.java

--- src/main/java/exloran/silvera/key/Keybinds.java
+++ src/main/java/exloran/silvera/key/Keybinds.java
@@ -1,6 +1,7 @@
-package com.silvera.client.key;
+package exloran.silvera.client.key;
 
 public class Keybinds {
+
     public static void register() {
         System.out.println("Keybind Loaded");
     }
