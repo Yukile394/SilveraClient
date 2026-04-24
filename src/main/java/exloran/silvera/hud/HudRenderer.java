1194b0eb5fbcb5fb04eca848464f9d55b24469be
Yukile394 <muhamedmuhet@gmail.com>
2026-04-24 03:09:38 +0300

    Refactor HUD registration to include ParticleHud

--- src/main/java/exloran/silvera/hud/HudRenderer.java
+++ src/main/java/exloran/silvera/hud/HudRenderer.java
@@ -1,22 +1,19 @@
 package com.silvera.client.hud;
 
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.math.MatrixStack;
 
 public class HudRenderer {
 
     public static void register() {
-        HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
-            MinecraftClient mc = MinecraftClient.getInstance();
 
-            drawContext.drawText(
-                mc.textRenderer,
-                "Silvera Client",
-                5,
-                5,
-                0xFFFFFF,
-                true
-            );
+        ParticleHud.init();
+
+        HudRenderCallback.EVENT.register((matrices, tickDelta) -> {
+            ParticleHud.tick();
+            ParticleHud.render(matrices);
         });
+
+        System.out.println("HUD Registered");
     }
 }
