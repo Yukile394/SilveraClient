93c21dbdb1729195651573d89d081dccb8c8d188
Yukile394 <muhamedmuhet@gmail.com>
2026-05-03 18:31:05 +0300

    Update SilveraConfig.java

--- src/main/java/exloran/silvera/config/SilveraConfig.java
+++ src/main/java/exloran/silvera/config/SilveraConfig.java
@@ -1,36 +1,23 @@
 package com.silvera.client.config;
 
 public class SilveraConfig {
-
-    // HUD Açık / Kapalı
+    // Mevcut ayarlar
     public static boolean hudEnabled = true;
-
-    // Keystrokes
     public static boolean keystrokesEnabled = true;
-
-    // CPS Counter
     public static boolean cpsEnabled = true;
-
-    // ArrayList
     public static boolean arrayListEnabled = true;
-
-    // Renk
     public static int red = 255;
     public static int green = 255;
     public static int blue = 255;
-
-    // HUD Pozisyon
     public static int hudX = 5;
     public static int hudY = 5;
-
-    // FPS Göster
     public static boolean fpsEnabled = true;
-
-    // Ping Göster
     public static boolean pingEnabled = false;
-
-    // Watermark Text
     public static String watermark = "Silvera Client";
+
+    // Yeni Özellikler (Aura & Elytra)
+    public static boolean auraEnabled = false;
+    public static boolean elytraTargetEnabled = false;
 
     public static void load() {
         System.out.println("Silvera Config Loaded");
@@ -40,3 +27,4 @@ public class SilveraConfig {
         System.out.println("Silvera Config Saved");
     }
 }
+
